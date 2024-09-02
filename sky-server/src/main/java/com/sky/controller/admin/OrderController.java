b041c43c29f81f36f67154854d46d22e86a7e875
姚文康 <by7750@qq.com>
2024-09-02 10:37:42 +0800

    管理端接单功能代码

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
@@ -10,10 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 管理端订单接口
@@ -62,9 +60,17 @@ public class OrderController {
 
     @ApiOperation("查询订单详情")
     @GetMapping("/details/{id}")
-    public Result<OrderVO> detail(@PathVariable Long id){
+    public Result<OrderVO> detail(@PathVariable Long id) {
         log.info("管理员查询订单详情：{}", id);
         return Result.success(orderService.details(id));
+    }
+
+    @ApiOperation("接单")
+    @PutMapping("/confirm")
+    public Result confirm(@RequestBody OrdersConfirmDTO ordersConfirmDTO) {
+        log.info("开始接单：{}", ordersConfirmDTO);
+        orderService.confirm(ordersConfirmDTO);
+        return Result.success();
     }
 
 }
