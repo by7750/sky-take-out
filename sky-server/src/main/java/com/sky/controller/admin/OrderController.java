58ebc00f2bdf4c9553c1fac7b5e4be33e9ab96f8
姚文康 <by7750@qq.com>
2024-09-02 09:45:42 +0800

    管理端查询订单详情功能代码

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,11 +5,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,6 +58,13 @@ public class OrderController {
         log.info("查询各个状态订单数量统计");
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
         return Result.success(orderStatisticsVO);
+    }
+
+    @ApiOperation("查询订单详情")
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> detail(@PathVariable Long id){
+        log.info("管理员查询订单详情：{}", id);
+        return Result.success(orderService.details(id));
     }
 
 }
